10047ba025ad6855167ebc13057e3cef75718d92
Christoph Moeller <christoph.moeller@disroot.org>
2022-02-12 00:25:47 +0900

    types1

--- firstp.go
+++ firstp.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println(2+2 != 5)
+	fmt.Println(reflect.TypeOf(4.0))
 }
+
+/*Go is statically typed -> knows types of vars before running*/
