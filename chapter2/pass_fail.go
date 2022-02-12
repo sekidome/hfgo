5678c686ded6286a734ba87fcf8bc33ee5584e57
Christoph Moeller <christoph.moeller@disroot.org>
2022-02-13 00:26:34 +0900

    error handling, trimming whitespaces and converting string to int

--- chapter2/pass_fail.go
+++ chapter2/pass_fail.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 // this is a comment
 func main() {
-	testResult := 56
-	if testResult > 60 {
+	fmt.Print("Enter a grade:")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n') // ReadString method requires an argument with a rune that marks the end of the input
+	if err != nil {
+		log.Fatal(err)
+	}
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64)
+	fmt.Println(grade)
+	if grade > 60 {
 		fmt.Println("Passed")
 	} else {
 		fmt.Println("Failed")
